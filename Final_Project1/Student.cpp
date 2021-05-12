34bd3248fb07dfd0a8203bb71848cb6d5fb40902
xBubblesx <84062055+xBubblesx@users.noreply.github.com>
2021-05-11 23:34:14 -0600

    Update Student.cpp

--- Final_Project1/Student.cpp
+++ Final_Project1/Student.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 #include <QString>
 #include"Student.h"
-//student class
 
+//Initializes the student constructor
 Student::Student(const QString &id, const QString &fName, const QString &lName, const QString &pw, const QString &email, const QString &pNo, const QString &gender, const QString &birthDate)
     :User(id, fName, lName, pw, email, pNo, gender, birthDate)
 {
